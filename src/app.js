dbd809f2485143d712a00d7e83ca06857ca868e6
MartinAcevedoThomas <martin.thomas.acevedo2003@gmail.com>
2022-05-12 00:14:06 +0200

    Finale

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const server = require("http").Server(app);
+const io = require('socket.io')(server);
+const stream = require('./ws/stream');
+const path = require('path');
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.of('/stream').on('connection', stream);
+
+server.listen(3030, () => {
+    console.log("Server listening on port 3030!")
+});
