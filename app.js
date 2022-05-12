2d6584d0f57896380becbdef9a84b7b6baca2a8f
MartinAcevedoThomas <martin.thomas.acevedo2003@gmail.com>
2022-05-12 10:18:26 +0200

    a

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const server = require("http").Server(app);
 const io = require('socket.io')(server);
-const stream = require('./ws/stream');
+const stream = require("./ws/stream");
 const path = require('path');
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('./assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
 
 io.of('/stream').on('connection', stream);
 
-server.listen(3030, () => {
+server.listen(80, () => {
     console.log("Server listening on port 3030!")
 });
